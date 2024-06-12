060ea45ad9f7f2139666e017b2fcc0e1b93724ef
akulikoff <akulikoff86@gmail.com>
2024-06-12 17:19:57 +1000

    init

---
+++ src/components/Button.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+interface ButtonProps {
+  label: string;
+  iconPath?: string;
+  //   size: {default: "small"; large: "large"}
+}
+defineProps<ButtonProps>();
+</script>
+
+<template>
+  <div class="flex-auto flex-row">
+    <button type="button" class="flex flex-row items-center">
+      {{ label }}
+      <span v-if="iconPath" class="ml-4">
+        <svg
+          aria-hidden="true"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+          class="w-6 h-6"
+        >
+          <path :d="iconPath"></path>
+        </svg>
+      </span>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
